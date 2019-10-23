8b10b0a34d35fc191a792854eca4063408060c84
Muhanned <muhannedkamil2005@gmail.com>
2019-10-22 23:39:20 -0400

    Create Components folder and build Header.js Component

---
+++ Guess-Number/components/Header.js
@@ -0,0 +1,16 @@
+import react from 'react'
+
+import {View,StyleSheet,Text} from 'react-native'
+
+const Header = props =>{
+
+    return(
+        <View></View>
+    )
+}
+
+const style  =StyleSheet.create({
+
+});
+
+export default Header;
