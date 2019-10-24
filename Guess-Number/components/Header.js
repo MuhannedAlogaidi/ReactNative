a2844038f6d5276bbf1d4f0797147b6812656eb1
Muhanned <muhannedkamil2005@gmail.com>
2019-10-23 23:54:10 -0400

    Build Header component

--- Guess-Number/components/Header.js
+++ Guess-Number/components/Header.js
@@ -1,16 +1,27 @@
-import react from 'react'
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-import {View,StyleSheet,Text} from 'react-native'
+const Header = props => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerTitle}>{props.title}</Text>
+    </View>
+  );
+};
 
-const Header = props =>{
-
-    return(
-        <View></View>
-    )
-}
-
-const style  =StyleSheet.create({
-
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    height: 90,
+    paddingTop: 36,
+    backgroundColor: '#f7287b',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  headerTitle: {
+    color: 'black',
+    fontSize: 18
+  }
 });
 
 export default Header;
