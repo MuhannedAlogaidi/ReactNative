dbeb50200763bcd0b1905ddcfb5b44cdae5e24a5
Muhanned <muhannedkamil2005@gmail.com>
2019-12-18 18:41:35 -0500

    Clear the App.js and make it standard

--- shopping-app-ui/App.js
+++ shopping-app-ui/App.js
@@ -4,16 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>A new application</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
