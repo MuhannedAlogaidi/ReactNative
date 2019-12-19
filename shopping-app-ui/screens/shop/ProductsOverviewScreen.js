681466240b0e2f149558773e9f583263d2474d76
Muhanned <muhannedkamil2005@gmail.com>
2019-12-18 23:27:51 -0500

    Initiate ProductsOverviewScreen  . this commit belong to prevoius one

---
+++ shopping-app-ui/screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { FlatList, Text } from 'react-native';
+
+const ProductsOverviewScreen = props => {
+  return <FlatList />;
+};
+
+export default ProductsOverviewScreen;
